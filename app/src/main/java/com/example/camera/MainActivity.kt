ccd38bc3294ecd4fe413572631c627ecc7c204b2
Kyle Tolentino <kyltol@gmail.com>
2021-01-17 09:31:49 -0600

    implemented recognizer, rearranged xml, updated gradle

--- app/src/main/java/com/example/camera/MainActivity.kt
+++ app/src/main/java/com/example/camera/MainActivity.kt
@@ -6,25 +6,82 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.util.concurrent.Executors
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
+import com.google.mlkit.vision.text.TextRecognizer
+import com.google.mlkit.vision.text.TextRecognition
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.nio.ByteBuffer
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
-typealias LumaListener = (luma: Double) -> Unit
+
+/*
+    Credit to CameraX and ML Kit documentation and tutorials
+    https://codelabs.developers.google.com/codelabs/camerax-getting-started#0
+    https://developers.google.com/ml-kit/vision/text-recognition/android#2.-get-an-instance-of-textrecognizer
+ */
 
 class MainActivity : AppCompatActivity() {
     private var imageCapture: ImageCapture? = null
 
     private lateinit var outputDirectory: File
     private lateinit var cameraExecutor: ExecutorService
+
+    private class TextRecognizerAnalyzer(private var ocrTextView: TextView) : ImageAnalysis.Analyzer {
+
+        private val recognizer = TextRecognition.getClient()
+
+        @androidx.camera.core.ExperimentalGetImage
+        override fun analyze(imageProxy: ImageProxy) {
+            val mediaImage = imageProxy.image ?: return
+
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+            // Pass image to an ML Kit Vision API
+            val result = recognizer.process(image)
+                    .addOnSuccessListener { visionText ->
+                        // Task completed successfully
+                        Log.d(TAG, "RECOGNIZED DIGIT")
+                        ocrTextView.text = visionText.text
+                    }
+                    .addOnFailureListener { exception ->
+                        // Task failed with an exception
+                        Log.e(TAG, "Text recognition error", exception)
+                    }
+                    .addOnCompleteListener {
+                        imageProxy.close()
+                    }
+        }
+
+        private fun processTextBlock(result: Text) {
+            // [START mlkit_process_text_block]
+            val resultText = result.text
+            for (block in result.textBlocks) {
+                val blockText = block.text
+                val blockCornerPoints = block.cornerPoints
+                val blockFrame = block.boundingBox
+                for (line in block.lines) {
+                    val lineText = line.text
+                    val lineCornerPoints = line.cornerPoints
+                    val lineFrame = line.boundingBox
+                    for (element in line.elements) {
+                        val elementText = element.text
+                        val elementCornerPoints = element.cornerPoints
+                        val elementFrame = element.boundingBox
+                    }
+                }
+            }
+            // [END mlkit_process_text_block]
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,13 +118,21 @@ class MainActivity : AppCompatActivity() {
             // Select back camera as a default
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
+            // instantiate an instance of ImageAnalyzer
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, TextRecognizerAnalyzer(sample_ocr_text))
+                }
+
+
             try {
                 // Unbind use cases before rebinding
                 cameraProvider.unbindAll()
 
                 // Bind use cases to camera
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview)
+                    this, cameraSelector, preview, imageAnalyzer)
 
             } catch(exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
